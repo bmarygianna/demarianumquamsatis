17f1b979fda0a82da6386dbcfbf2ef0b28ca3959
Konopelski <konopelski@Konopelskis-MacBook-Air.local>
2025-11-05 21:17:50 -0500

    Retheme site for De Marianum Quam Satis

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "De Marianum Quam Satis";
+export const SITE_DESCRIPTION =
+	"Notes on Marian devotion, Catholic imagination, and the quiet work of contemplation.";
